74894387688fedf59cc42a8870bfe2679412aad8
nikhilkumbhare <nikhilk190697@gmail.com>
2021-02-21 12:47:51 +0530

    Added: Part Time Employee and Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,15 +4,19 @@ public class EmployeeWage {
    public static void main (String[] args) {
 		//Constants
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int WAGE_PER_HR = 20;
 		//Variables
 		int empHrs = 0;
 		int dailyWage = 0;
 		//Computation
-		int empCheck = (int) Math.floor(Math.random() *10) % 2;
+		int empCheck = (int) Math.floor(Math.random() *10) % 3;
 		if (empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is Present");
+			System.out.println("Employee is Present Full Time");
 			empHrs=8;
+		}else if (empCheck == IS_PART_TIME) {
+			System.out.println("Employee is Present Part Time");
+			empHrs=4;
 		}else {
 			System.out.println("Employee is Absent");
 			empHrs=0;
